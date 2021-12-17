6afcc9d9dac38ade49fa1d0be6d778cf6a710e63
Romira915 <40430090+Romira915@users.noreply.github.com>
2021-12-17 18:47:43 +0900

    standard algorithm ok

--- main.cpp
+++ main.cpp
@@ -1,25 +1,55 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <string>
 
-int main()
+using namespace std;
+
+string standard(const uint32_t N, const uint32_t A[], const uint64_t target_size)
 {
-    int N = 0;
-    scanf("%d", &N);
-    int *A = new int[N * 2];
-    int sum = 0;
+    uint64_t count_size = 0;
 
     for (int i = 0; i < N; i++)
     {
-        scanf("%d", &A[i]);
+        count_size = 0;
+        for (int j = i; j < N * 2; j++)
+        {
+            count_size += A[j];
+            if (count_size == target_size)
+            {
+                return string("Yes");
+            }
+            if (count_size > target_size)
+            {
+                break;
+            }
+        }
+    }
+
+    return string("No");
+}
+
+int main()
+{
+    uint32_t N = 0;
+
+    scanf("%u", &N);
+
+    uint32_t *A = new uint32_t[N * 2];
+    uint64_t sum = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%u", &A[i]);
         A[i + N] = A[i];
         sum += A[i];
     }
 
-    int target_size = sum / 10;
-    int count_size = 0;
+    const uint64_t target_size = sum / 10;
 
-    for (int i = 0; i < N; i++)
-    {
-    }
+    string decision = standard(N, A, target_size);
+    printf("%s", decision.c_str());
+
+    delete[] A;
 
     return 0;
 }
